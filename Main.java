9d9f10fde35950415634240b59413de1b96bd60f
Vinícios Grama <vinyirun1@gmail.com>
2019-10-01 00:05:54 -0300

    :rocket: Cria switch de opções

--- Main.java
+++ Main.java
@@ -1,17 +1,42 @@
-package estacionamento;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Carro c;
-        Pilha p = new Pilha();
+        Estacionamento e = new Estacionamento();
+        Scanner teclado=new Scanner (System.in);
         
-        p.estacionar(new Carro("JOS"));
-        p.estacionar(new Carro("BRASIL"));
-        p.estacionar(new Carro("TESTE"));
-        p.estacionar(new Carro("BRA"));
-        p.retirarCarro("JOS");
-        p.retirarCarro("BRASIL");
-        p.retirarCarro("TESTE");
-        p.retirarCarro("BRA");
-    }
+        System.out.println("0 - Sair  \n"
+                + "1 - Estacionar carro  \n"
+                + "2 - Retirar carro do estacionamento  \n"
+                + "3 - Ver todos carros estacionados \n"
+                + "4 - Mostrar carro na saída");
+        System.out.println("\n**************************** ESTACIONAMENTO *****************************");
+        String valor;
+        int opcao=1;
+        while (opcao!=0){
+            System.out.println("Digite sua opcao:");
+            opcao=teclado.nextInt();
+            teclado.nextLine();
+            switch(opcao){
+            case 1:
+                System.out.println("Digite a placa do carro a ser estacionado: ");
+                valor=teclado.nextLine();
+                e.estacionar(new Carro(valor));
+                break;
+            case 2:
+                System.out.println("Digite a placa do carro a ser retirado: ");
+                valor=teclado.nextLine();
+                e.retirarCarro(valor);
+                break;
+            case 3:
+                // e.imprimir();
+                break;
+            case 4:
+                e.imprimirTopo();
+                break;
+            }
+        } 
+            System.exit(0);
+        }
 }
